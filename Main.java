ebb8871228e774ff1170e52607b7425da84da7c5
mirjalol <mirislomovmirjalol@gmail.com>
2023-01-23 12:46:04 +0000

    removed un used code from main object

--- Main.java
+++ Main.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Data data = new Data();
-        data.readUsersFromFile();
-
-
-        Response response = new Response();
-
         Response.welcoming();
         Menu.mainMenu();
     }
